51735d3e7835085f3d8584c55e3ce4a2cacf7aeb
adbenitez <asieldbenitez@gmail.com>
2023-01-20 20:56:36 -0500

    update simplebot_aio/__init__.py

--- simplebot_aio/__init__.py
+++ simplebot_aio/__init__.py
@@ -1,5 +1,6 @@
 """Asynchronous library to help with Delta Chat bot development"""
 # pylama:ignore=W0611,W0401
 from deltachat_rpc_client import *
+from deltachat_rpc_client import const, events
 
 from .cli import BotCli
