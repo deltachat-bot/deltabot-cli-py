79c743e459d539c3ad44302f62dced62eb00541e
adbenitez <asieldbenitez@gmail.com>
2023-02-12 10:26:06 -0500

    remove run_in_background()

--- simplebot_aio/__init__.py
+++ simplebot_aio/__init__.py
@@ -3,5 +3,4 @@
 from deltachat_rpc_client import *
 from deltachat_rpc_client import const, events
 
-from ._utils import run_in_background
 from .cli import BotCli
--- simplebot_aio/_utils.py
+++ simplebot_aio/_utils.py
@@ -1,19 +1,5 @@
 """Utilities"""
-import asyncio
-from typing import Coroutine
-
 from rich.progress import track
-
-_background_tasks = set()
-
-
-def run_in_background(coro: Coroutine) -> None:
-    """Schedule the execution of a coroutine object in a spawn task, keeping a
-    reference to the task to avoid it disappearing mid-execution due to GC.
-    """
-    task = asyncio.create_task(coro)
-    _background_tasks.add(task)
-    task.add_done_callback(_background_tasks.discard)
 
 
 class ConfigProgressBar:
