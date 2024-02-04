fd1b6abc6c0457bce748b0f6e42d2e341e3ff2e8
adbenitez <asieldbenitez@gmail.com>
2024-02-04 16:01:46 -0500

    update advanced example

--- examples/echobot_advanced.py
+++ examples/echobot_advanced.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Advanced echo bot example."""
-from deltabot_cli import BotCli, EventType, events
+from deltabot_cli import BotCli, EventType, events, is_not_known_command
 
 cli = BotCli("echobot")
 
@@ -15,15 +15,27 @@ def log_event(bot, _accid, event):
         bot.logger.error(event.msg)
 
 
-@cli.on(events.NewMessage)
+@cli.on(events.NewMessage(func=is_not_known_command))
 def echo(bot, accid, event):
     msg = event.msg
     bot.rpc.misc_send_text_message(accid, msg.chat_id, msg.text)
 
 
+@cli.on(events.NewMessage(command="/help"))
+def _help(bot, accid, event):
+    msg = event.msg
+    bot.rpc.send_msg(accid, msg.chat_id, {"text": "I will repeat anything you say to me"})
+
+
 @cli.on_init
 def on_init(bot, args):
-    bot.logger.info("Initializing bot with args: %s", args)
+    bot.logger.info("Initializing CLI with args: %s", args)
+    for accid in bot.rpc.get_all_account_ids():
+        if not bot.rpc.get_config(accid, "displayname"):
+            bot.rpc.set_config(accid, "displayname", "EchoBot")
+            status = "I am a Delta Chat bot, I will repeat anything you say to me"
+            bot.rpc.set_config(accid, "selfstatus", status)
+            bot.rpc.set_config(accid, "delete_server_after", "1")  # delete immediately from server
 
 
 @cli.on_start
